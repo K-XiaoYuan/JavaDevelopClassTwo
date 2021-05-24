176bdedbe988e6628c0a19c0f0e4d005c8f5d5f0
xiaoyuan <2235760845@qq.com>
2021-05-24 22:06:37 +0800

    opmp项目的初始化环境设置，包含了通用的忽略上传文件的规则

---
+++ src/main/java/cn/nnxy/opmp/OpmpApplication.java
@@ -0,0 +1,13 @@
+package cn.nnxy.opmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/nnxy/opmp/OpmpApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nnxy.opmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
